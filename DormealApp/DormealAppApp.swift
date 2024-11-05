3c2597e683b977c334fda60647a582b7c23a612d
Anthony Campos <anthonyrubencampos@gmail.com>
2024-11-04 23:37:46 -0500

    Initial Commit

---
+++ DormealApp/DormealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormealAppApp.swift
+//  DormealApp
+//
+//  Created by Anthony Campos on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DormealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DormealAppTests/DormealAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DormealAppTests.swift
+//  DormealAppTests
+//
+//  Created by Anthony Campos on 11/4/24.
+//
+
+import Testing
+@testable import DormealApp
+
+struct DormealAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
